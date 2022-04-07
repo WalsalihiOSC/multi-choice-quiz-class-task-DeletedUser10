17d0a086b7b063d268a0e467169debf083dc2a33
Ayush Kumar <90948894+DeletedUser10@users.noreply.github.com>
2022-04-08 09:35:25 +1200

    second class created
    Created a second class that returns the correct answer to the check answer method

--- multi_choice_quiz.py
+++ multi_choice_quiz.py
@@ -3,8 +3,10 @@
 
 from tkinter import*
 
-class MultiChoice:
+class Interface:
     def __init__(self, parent):
+        self.quiz = Quiz()
+
         main_frame = Frame(parent)
         main_frame.grid()
         
@@ -13,30 +15,39 @@ class MultiChoice:
 
         ques = Label(f1, text = "What is the capital of New Zealand?")
         ques.grid()
-        self.ans = ["Manurewa", "Auckland", "Wellington", "Botany", "Mt Wellington"]
         self.ans_var= StringVar()
-        self.ans_var.set(self.ans)
+        self.ans_var.set(self.quiz.ans)
 
-        for answer in self.ans:
+        for answer in self.quiz.ans:
             Radiobutton(f1, text = answer, variable=self.ans_var,
                                         value= answer,
-                                        command=self.show_label).grid(sticky=W)
+                                        command=self.check_answer).grid(sticky=W)
 
         self.check = Label(main_frame, text = " ")
         self.check.grid(column=1, sticky=W)
-    def show_label(self):
-        if self.ans_var.get() == self.ans[2]:
-            self.check.configure(text = "correct")
+
+        
+    def check_answer(self):
+        if self.quiz.is_answer_correct(self.ans_var.get()):
+            self.check.configure(text = "Correct", font = "Arial")
         else:
-            self.check.configure(text = "Incorrect")
+            self.check.configure(text = "Incorrect", font = "Arial")
+
             
-
-
+        
+"""Second class that returns the correct answer"""
+class Quiz:
+    
+    def __init__(self):
+        self.ans = ["Manurewa", "Auckland", "Wellington", "Botany", "Mt Wellington"]
+    
+    def is_answer_correct(self,answer):
+        return answer == self.ans[2]
 
 
 def main():
     root = Tk()
-    MultiChoice(root)
+    Interface(root)
     root.mainloop()
  
 main()
